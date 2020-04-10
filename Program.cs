5c9c569a3659649cdde6a9873ce6abcec64152ce
shohincoding-com <saomov.shokhin@gmail.com>
2020-04-10 17:49:22 +0500

    first part homework-test

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int N;
+            N = int.Parse(Console.ReadLine());
+            int[] arrey = new int[N];
+            for(int i = 0; i < arrey.Length; i++){
+                arrey[i]=new Random().Next(50);
+                Console.Write($"{arrey[i]} ");
+            }
+            
+        }
+    }
+}
